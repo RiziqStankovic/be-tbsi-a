352eea429a06138ecbfcfa2b9570d36768656726
rahman-autentika <rahman@autentika.id>
2023-09-06 10:41:11 +0700

    add docker worload and integration test

--- backend-a.js
+++ backend-a.js
@@ -3,9 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Backend A!');
+  res.send('Hello from Backend A, Versi 1.0.0!');
 });
 
 app.listen(port, () => {
   console.log(`Backend A is running on port ${port}`);
 });
+
+// Export the 'app' object so it can be used in tests
+module.exports = app;
